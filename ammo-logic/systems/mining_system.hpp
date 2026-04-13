c42dac973a06728125a00a790accdf2ec2de2dc7
JaJaLys <sylvain.janniere@laposte.net>
2026-04-13 21:33:38 +0200

    split gather/craft resources

--- ammo-logic/systems/mining_system.hpp
+++ ammo-logic/systems/mining_system.hpp
@@ -1,25 +1,34 @@
 #ifndef _MINING_SYSTEM_HPP
 #define _MINING_SYSTEM_HPP 1
 
-class MiningSystem: public CraftOrderSystem
+class MiningGatheringSystem: public CraftOrderSystem
 {
 public:
-    MiningSystem(ItemCraftingManager& item_crafting_manager, InventoryManager& bank) :
-        CraftOrderSystem("MiningSystem", item_crafting_manager, bank)
+    MiningGatheringSystem(ItemCraftingManager& item_crafting_manager, InventoryManager& bank) :
+        CraftOrderSystem("MiningGatheringSystem", item_crafting_manager, bank)
     {
         m_Items.push_back({ "copper_ore", 20, 500 });
-        m_Items.push_back({ "copper_bar", 5, 50 });
         m_Items.push_back({ "iron_ore", 20, 500 });
-        m_Items.push_back({ "iron_bar", 20, 50 });
         m_Items.push_back({ "coal", 20, 500 });
-        m_Items.push_back({ "steal_bar", 20, 50 });
         m_Items.push_back({ "gold_ore", 20, 500 });
-        m_Items.push_back({ "gold_bar", 20, 50 });
         m_Items.push_back({ "strange_ore", 20, 500 });
-        m_Items.push_back({ "strangold_bar", 20, 50 });
         m_Items.push_back({ "mithril_ore", 20, 500 });
-        m_Items.push_back({ "mithril_bar", 20, 50 });
         m_Items.push_back({ "adamantine_ore", 20, 500 });
+    }
+};
+
+class MiningCraftingSystem: public CraftOrderSystem
+{
+public:
+    MiningCraftingSystem(ItemCraftingManager& item_crafting_manager, InventoryManager& bank) :
+        CraftOrderSystem("MiningCraftingSystem", item_crafting_manager, bank)
+    {
+        m_Items.push_back({ "copper_bar", 5, 50 });
+        m_Items.push_back({ "iron_bar", 20, 50 });
+        m_Items.push_back({ "steal_bar", 20, 50 });
+        m_Items.push_back({ "gold_bar", 20, 50 });
+        m_Items.push_back({ "strangold_bar", 20, 50 });
+        m_Items.push_back({ "mithril_bar", 20, 50 });
         m_Items.push_back({ "adamantine_bar", 20, 50 });
     }
 };
