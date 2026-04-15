467238e3edb7046456793809ab262d42d592d023
JaJaLys <sylvain.janniere@laposte.net>
2026-04-15 13:20:04 +0200

    reorder

--- ammo-logic/systems/gather_system.hpp
+++ ammo-logic/systems/gather_system.hpp
@@ -32,14 +32,14 @@ class WoodcuttingGatheringSystem: public GatherSystem
 {
     WoodcuttingGatheringSystem() : GatherSystem("WoodcuttingGatheringSystem")
     {
-        m_Resources.push_back({ "ash_wood", 500 });
-        m_Resources.push_back({ "spruce_wood", 500 });
-        m_Resources.push_back({ "birch_wood", 500 });
-        m_Resources.push_back({ "hardwood_wood", 500 });
-        m_Resources.push_back({ "dead_wood", 500 });
-        m_Resources.push_back({ "magic_wood", 500 });
-        m_Resources.push_back({ "maple_wood", 500 });
         m_Resources.push_back({ "palm_wood", 500 });
+        m_Resources.push_back({ "maple_wood", 500 });
+        m_Resources.push_back({ "magic_wood", 500 });
+        m_Resources.push_back({ "dead_wood", 500 });
+        m_Resources.push_back({ "hardwood_wood", 500 });
+        m_Resources.push_back({ "birch_wood", 500 });
+        m_Resources.push_back({ "spruce_wood", 500 });
+        m_Resources.push_back({ "ash_wood", 500 });
 
         m_Equipements.push_back("voidstone_axe");
         m_Equipements.push_back("adamantite_axe");
@@ -58,13 +58,13 @@ class MiningGatheringSystem: public GatherSystem
 {
     MiningGatheringSystem() : GatherSystem("MiningGatheringSystem")
     {
-        m_Resources.push_back({ "copper_ore", 500 });
-        m_Resources.push_back({ "iron_ore", 500 });
-        m_Resources.push_back({ "coal", 500 });
-        m_Resources.push_back({ "gold_ore", 500 });
-        m_Resources.push_back({ "strange_ore", 500 });
-        m_Resources.push_back({ "mithril_ore", 500 });
         m_Resources.push_back({ "adamantine_ore", 500 });
+        m_Resources.push_back({ "mithril_ore", 500 });
+        m_Resources.push_back({ "strange_ore", 500 });
+        m_Resources.push_back({ "gold_ore", 500 });
+        m_Resources.push_back({ "coal", 500 });
+        m_Resources.push_back({ "iron_ore", 500 });
+        m_Resources.push_back({ "copper_ore", 500 });
 
         m_Equipements.push_back("voidstone_pickaxe");
         m_Equipements.push_back("adamantite_pickaxe");
@@ -83,10 +83,10 @@ class AlchemyGatheringSystem: public GatherSystem
 {
     AlchemyGatheringSystem() : GatherSystem("AlchemyGatheringSystem")
     {
-        m_Resources.push_back({ "sunflower", 500 });
-        m_Resources.push_back({ "nettle_leaf", 500 });
-        m_Resources.push_back({ "glowstem_leaf", 500 });
         m_Resources.push_back({ "torch_cactus_flower", 500 });
+        m_Resources.push_back({ "glowstem_leaf", 500 });
+        m_Resources.push_back({ "nettle_leaf", 500 });
+        m_Resources.push_back({ "sunflower", 500 });
 
         m_Equipements.push_back("voidstone_gloves");
         m_Equipements.push_back("adamantite_gloves");
@@ -105,12 +105,12 @@ class FishingGatherSystem: public GatherSystem
 {
     FishingGatherSystem() : GatherSystem("FishingGatherSystem")
     {
-        m_Resources.push_back({ "gudgeon", 500 });
-        m_Resources.push_back({ "shrimp", 500 });
-        m_Resources.push_back({ "trout", 500 });
-        m_Resources.push_back({ "bass", 500 });
-        m_Resources.push_back({ "salmon", 500 });
         m_Resources.push_back({ "swordfish", 500 });
+        m_Resources.push_back({ "salmon", 500 });
+        m_Resources.push_back({ "bass", 500 });
+        m_Resources.push_back({ "trout", 500 });
+        m_Resources.push_back({ "shrimp", 500 });
+        m_Resources.push_back({ "gudgeon", 500 });
 
         m_Equipements.push_back("voidstone_fishing_rod");
         m_Equipements.push_back("adamantite_fishing_rod");
